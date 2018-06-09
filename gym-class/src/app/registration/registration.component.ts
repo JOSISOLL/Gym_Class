109f861dbbb0d66266e501142359fa750ecf9322
Yoseph Solomon <3253217965@qq.com>
2018-06-09 18:47:15 +0300

    User session registration modified. Registration date and hour will be passed with other registration inforamtion.

--- gym-class/src/app/registration/registration.component.ts
+++ gym-class/src/app/registration/registration.component.ts
@@ -6,6 +6,7 @@ import { RegService } from '../reg.service';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {Subject} from 'rxjs';
 import {debounceTime} from 'rxjs/operators';
+import { ReactiveFormsModule, FormsModule, FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -14,20 +15,43 @@ import {debounceTime} from 'rxjs/operators';
 })
 export class RegistrationComponent implements OnInit {
 
+  myForm: FormGroup; 
+  username: FormControl;
+  sessionId: FormControl; 
+  phonenumber: FormControl; 
+  regDate: FormControl; 
+
   constructor(private _sessionService: SessionService, private _router: Router, private _regService: RegService) { }
-  regData = {}
+  regData = []
   regSessions = []
 
   private _success = new Subject<string>();
   staticAlertClosed = false;
   successMessage: string;
   todays_session = []
-  date : string
+  date : Date
   private today = new Date().toLocaleString('en-us', {  weekday: 'long' })
 
 
+  createControls(){
+    this.username = new FormControl()
+    this.sessionId = new FormControl()
+    this.phonenumber = new FormControl()
+    this.regDate = new FormControl()
+  }
+  createForm(){
+    this.myForm = new FormGroup({
+      username : this.username,
+      phonenumber : this.phonenumber,
+      sessionId : this.sessionId,
+      regDate : this.regDate
+    })
+  }
+
   ngOnInit() {
-    this.date = new Date().toLocaleString()
+    this.createControls()
+    this.createForm()
+    this.date = new Date()
     this._sessionService.getRegisterSessions()
     .subscribe(
       res => {
@@ -58,15 +82,21 @@ export class RegistrationComponent implements OnInit {
   }
 
   registration(){
-    console.log(this.regData)
-    this._regService.regSession(this.regData)
-    .subscribe(
-      res => {
-        console.log(res.username)
-        this._success.next(` Dear ${res.username}, you have successfully registered for this session. Make sure to confirm 1 hour before the session starts.`);
-      },
-      err => console.log(err)
-    )
+    if(this.myForm.valid){
+      this.regData = this.myForm.value
+      console.log(this.regData)
+      this._regService.regSession(this.regData)
+      .subscribe(
+        res => {
+          console.log(res.username)
+          this._success.next(` Dear ${res.username}, you have successfully registered for this session. Make sure to confirm 1 hour before the session starts.`);
+        },
+        err => console.log(err)
+      )
+    }
+    else{
+      console.log("FORM INVALID")
+    }
   }
   
 
--- server/models/registration.js
+++ server/models/registration.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema
 const regSchema = new Schema({
     sessionId: String,
     username: String,
-    phonenumber : String
+    phonenumber : String,
+    regDate: Date
 })
 
 module.exports = mongoose.model('reg', regSchema, 'session_reg')
